6a4c52b7b4ae0ac2d38f3fc6dfff1ba2e02ee943
minotaur <minotaur-ergo@protonmail.com>
2022-03-17 12:53:01 +0330

    share extended public key for future use

--- src/api/wallet/address/AddressList.tsx
+++ src/api/wallet/address/AddressList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Divider, List } from "@material-ui/core";
+import { Divider, List, ListItem, ListItemText } from "@material-ui/core";
 import AddressElement from "./AddressElement";
 import DeriveAddress from "./DeriveAddress";
 import { getWalletAddresses } from "../../../action/address";
@@ -7,23 +7,26 @@ import BottomSheet from "../../../components/bottom-sheet/BottomSheet";
 import AddressView from "./AddressView";
 import AddressWithErg from "../../../db/entities/views/AddressWithErg";
 import { WalletPagePropsType } from "../../../utils/interface";
+import ExtendedPublicKeyView from "./ExtendedPublicKeyView";
 
 interface StateType {
     addresses: Array<AddressWithErg>;
     selectedAddr: AddressWithErg | null;
     showAddress: boolean;
     addressValid: boolean;
+    showExtended: boolean;
 }
 
 class AddressList extends React.Component<WalletPagePropsType, StateType> {
-    state = {
+    state: StateType = {
         addresses: [],
         selectedAddr: null,
         showAddress: false,
         addressValid: false,
-    }
+        showExtended: false
+    };
     load_data = () => {
-        if(!this.state.addressValid){
+        if (!this.state.addressValid) {
             getWalletAddresses(this.props.wallet.id).then(dbAddresses => {
                 this.setState({
                     addresses: dbAddresses,
@@ -31,32 +34,44 @@ class AddressList extends React.Component<WalletPagePropsType, StateType> {
                 });
             });
         }
-    }
+    };
     componentDidMount = () => {
         this.props.setTab("address");
         this.load_data();
-    }
+    };
 
     componentDidUpdate = () => {
         this.load_data();
-    }
+    };
 
     addressDerived = () => {
-        this.setState({addressValid: false});
-    }
+        this.setState({ addressValid: false });
+    };
 
     showAddress = (address: AddressWithErg) => {
         this.setState({
             selectedAddr: address,
             showAddress: true
-        })
-    }
+        });
+    };
+
+    showPublicKey = () => {
+        this.setState({
+            showExtended: true
+        });
+    };
 
     closeAddress = () => {
         this.setState({
             showAddress: false
-        })
-    }
+        });
+    };
+
+    closeExtended = () => {
+        this.setState({
+            showExtended: false
+        });
+    };
 
     render = () => {
         const selectedAddress: AddressWithErg | null = this.state.selectedAddr;
@@ -68,6 +83,14 @@ class AddressList extends React.Component<WalletPagePropsType, StateType> {
                         addressDerived={this.addressDerived} />
                 ) : null}
                 <List>
+                    {!!this.props.wallet.extended_public_key.trim() ? (
+                        <React.Fragment>
+                            <Divider />
+                            <ListItem onClick={this.showPublicKey}>
+                                <ListItemText primary={"Display Extended public key"} />
+                            </ListItem>
+                        </React.Fragment>
+                    ) : null}
                     {this.state.addresses.map((address: AddressWithErg) => (
                         <React.Fragment key={address.id}>
                             <Divider />
@@ -78,7 +101,7 @@ class AddressList extends React.Component<WalletPagePropsType, StateType> {
                                 id={address.id}
                                 name={address.name}
                                 erg={address.erg()}
-                                token_count={address.token_count}/>
+                                token_count={address.token_count} />
                         </React.Fragment>
                     ))}
                 </List>
@@ -86,12 +109,21 @@ class AddressList extends React.Component<WalletPagePropsType, StateType> {
                     show={this.state.selectedAddr !== null && this.state.showAddress}
                     close={() => this.closeAddress()}>
                     {selectedAddress !== null ? (
-                        <AddressView address={selectedAddress} invalidate={() => this.setState({addressValid: false})} />
+                        <AddressView address={selectedAddress}
+                                     invalidate={() => this.setState({ addressValid: false })} />
                     ) : null}
                 </BottomSheet>
+                {this.props.wallet.extended_public_key.trim() ? (
+                    <BottomSheet
+                        show={this.state.showExtended}
+                        close={() => this.closeExtended()}>
+                        <ExtendedPublicKeyView extended={this.props.wallet.extended_public_key} />
+                    </BottomSheet>
+                ) : null}
             </React.Fragment>
-        )
+        );
     }
+    ;
 }
 
 export default AddressList;
