ef5198ca162f04acf654e4150216e1047abff195
shirinbehnami <hhagh2002@gmail.com>
2022-08-18 15:43:01 +0430

    stepForward function implemented.

--- src/action/sync.ts
+++ src/action/sync.ts
@@ -1,14 +1,25 @@
+import {BlockDbAction} from "./db";
+import { NetworkType, getNetworkType} from "../util/network_type";
+import { Paging } from "../util/network/paging";
+
 type Block = {
     id:string,
     height:number
 }
+const network_type: NetworkType = getNetworkType("Testnet")
+
 export function insertToDB(block : Block):void {
-    
+    BlockDbAction.InsertHeaders(Object.entries(block).map(value => {
+        return { id: block.id, height: block.height };
+    }), "Testnet");   
 }
 
 export async function stepForward():Promise<void>{
-    const block: Block = {
-        id: '3',
-        height: 0
-    };
+    let paging: Paging = { offset: 0, limit: 1 };
+    const recieveditem = (await network_type.getExplorer().getBlocksHeaders(paging)).items;
+    const recievedBlock: Block = {
+        id: recieveditem[0].id,
+        height: recieveditem[0].height
+    }
+    insertToDB(recievedBlock)
 };
