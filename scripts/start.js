bb7984a96f54a5a7ec95d005ed4cbcbca9cb3e97
vorujack <vorujack@gmail.com>
2022-10-03 19:02:04 +0330

    fix build scripts

--- scripts/start.js
+++ scripts/start.js
@@ -1,13 +1,13 @@
 // in ./build.js
-import rewire from 'rewire';
+const rewire = require('rewire');
 const defaults = rewire('react-scripts/scripts/start.js');
 const oldConfigFactory = defaults.__get__('configFactory');
-import updateConfig from './update-config';
+const updateConfig = require('./update-config');
 
 // required to update configs
 const configFactory = (webpackEnv) => {
-  let config = oldConfigFactory(webpackEnv);
-  return updateConfig(config);
-};
+    let config = oldConfigFactory(webpackEnv);
+    return updateConfig(config)
+}
 
-defaults.__set__('configFactory', configFactory);
+defaults.__set__('configFactory', configFactory)
