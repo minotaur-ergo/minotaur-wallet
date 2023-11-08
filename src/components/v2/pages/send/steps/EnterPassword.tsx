eb8bc96c96926708d4d0642ac2e8aadebad5f0da
Mojtaba Fazelinia <mfazelinia@gmail.com>
2023-11-08 19:15:06 +0330

    adds burning tokens

--- src/components/v2/pages/send/steps/EnterPassword.tsx
+++ src/components/v2/pages/send/steps/EnterPassword.tsx
@@ -1,6 +1,37 @@
 import React from 'react';
-import { Box, FormHelperText, Typography } from '@mui/material';
+import { Box, FormHelperText, Stack, Typography } from '@mui/material';
 import PasswordField from '../../../components/PasswordField';
+
+interface TokenItemPropsType {
+  amount: number;
+  name: string;
+  color?: 'primary' | 'success' | 'error';
+}
+
+function TokenItem({ amount, name, color }: TokenItemPropsType) {
+  return (
+    <Box display="flex" sx={{ gap: 1 }}>
+      <Box
+        sx={{
+          width: 6,
+          m: 0.5,
+          borderRadius: 4,
+          opacity: 0.5,
+          bgcolor:
+            color === 'success'
+              ? 'success.light'
+              : color === 'error'
+              ? 'error.light'
+              : 'primary.light',
+        }}
+      />
+      <Typography variant="body2" sx={{ flexGrow: 1 }}>
+        {name}
+      </Typography>
+      <Typography variant="body2">{amount.toFixed(2)}</Typography>
+    </Box>
+  );
+}
 
 export default function () {
   return (
@@ -17,6 +48,21 @@ export default function () {
       <FormHelperText sx={{ mb: 2 }}>
         These amount will be spent when transaction proceed.
       </FormHelperText>
+
+      <Typography variant="body2" color="textSecondary">
+        Burning tokens
+      </Typography>
+      <Stack sx={{ mb: 2, mt: 1 }} gap={0.5}>
+        <TokenItem name="Token 1" amount={10} color="error" />
+        <TokenItem name="Token 2" amount={5} color="error" />
+      </Stack>
+      <Typography variant="body2" color="textSecondary">
+        Issuing tokens
+      </Typography>
+      <Stack sx={{ mb: 2, mt: 1 }} gap={0.5}>
+        <TokenItem name="Token 1" amount={3} color="success" />
+        <TokenItem name="Token 2" amount={24} color="success" />
+      </Stack>
 
       <PasswordField
         label="Wallet Password"
