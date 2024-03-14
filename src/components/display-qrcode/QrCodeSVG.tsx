c56310eb744400de2186cc0b25cd47eb6d887452
vorujack <vorujack@gmail.com>
2024-03-14 05:46:37 +0330

    improve qrcode calculate size

--- src/components/display-qrcode/QrCodeSVG.tsx
+++ src/components/display-qrcode/QrCodeSVG.tsx
@@ -1,5 +1,5 @@
 import { QRCodeSVG as PackageQrCode } from 'qrcode.react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface QrCodeSVGPropsType {
   value: string;
@@ -7,23 +7,15 @@ interface QrCodeSVGPropsType {
 const QrCodeSVG = (props: QrCodeSVGPropsType) => {
   const [size, setSize] = useState(512);
   const [clientSize, setClientSize] = useState(0);
-  const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    if (ref && ref.current) {
-      const rect = ref.current.getBoundingClientRect();
-      const newSize = Math.min(rect.width, rect.height);
-      if (newSize !== clientSize) {
-        setClientSize(newSize);
-        const pow = Math.min(Math.floor(Math.log(newSize) / Math.log(2)), 9);
-        setSize(Math.pow(2, pow));
-      }
+    const newSize = Math.min(window.screen.width, window.screen.height);
+    if (newSize !== clientSize) {
+      setClientSize(newSize);
+      const pow = Math.min(Math.floor(Math.log(newSize) / Math.log(2)), 9);
+      setSize(Math.pow(2, pow));
     }
-  }, [size, ref, clientSize]);
-  return (
-    <div ref={ref}>
-      <PackageQrCode size={size} value={props.value} />
-    </div>
-  );
+  }, [size, clientSize]);
+  return <PackageQrCode size={size} value={props.value} />;
 };
 
 export default QrCodeSVG;
