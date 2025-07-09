3dc8988128ddf02b2219c5c7728d1a8802eeedbe
saeedehTorkamani <saeedehtorkamani@gmail.com>
2025-07-09 13:35:02 +0330

    Fix : Add validation in the impl_Fee box to enforce that the minimum value cannot be below wasm.BoxValue.SAFE_USER_MIN and prevent errors

--- apps/wallet/src/pages/wallet-page/dapps/apps/sigma-usd/parameters.ts
+++ apps/wallet/src/pages/wallet-page/dapps/apps/sigma-usd/parameters.ts
@@ -1,8 +1,14 @@
+import * as wasm from 'ergo-lib-wasm-browser';
+
 export const RESERVE_COIN_DEFAULT_PRICE = BigInt(1000000);
 export const MIN_BOX_VALUE = BigInt(10000000);
 
-export const IMPLEMENTOR_FEE = (amount: bigint) =>
-  (amount * BigInt(2)) / BigInt(1000);
+export const IMPLEMENTOR_FEE = (amount: bigint) => {
+  const value: bigint = (amount * BigInt(2)) / BigInt(1000);
+  const minValue = BigInt(wasm.BoxValue.SAFE_USER_MIN().as_i64().to_str());
+  return value < minValue ? minValue : value;
+};
+
 export const PROTOCOL_FEE = (amount: bigint) =>
   (amount * BigInt(2)) / BigInt(100);
 export const MIN_RESERVE_RATIO = BigInt(400);
