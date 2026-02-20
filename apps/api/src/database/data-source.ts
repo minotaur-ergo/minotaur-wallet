b9da3fde11f4b7a979517fa6e480102c7880f1e9
vorujack <vorujack@gmail.com>
2026-02-20 10:51:15 +0330

    add base routes

--- apps/api/src/database/data-source.ts
+++ apps/api/src/database/data-source.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 
-import { entities } from './entities/index.js';
+import { entities } from './entities';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
