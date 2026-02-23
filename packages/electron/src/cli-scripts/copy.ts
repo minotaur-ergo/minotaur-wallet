01e5f4cadf965fbe0ef352aff64cd6ff56c80aeb
Alireza Mousavi <alir.msvi@gmail.com>
2026-02-24 01:51:04 +0330

    add electron cli

---
+++ packages/electron/src/cli-scripts/copy.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { existsSync } from 'fs';
+import { copySync, removeSync } from 'fs-extra';
+import { join } from 'path';
+
+import type { TaskInfoProvider } from './common';
+
+export async function doCopy(
+  taskInfoMessageProvider: TaskInfoProvider,
+): Promise<void> {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const usersProjectDir = process.env.CAPACITOR_ROOT_DIR!;
+  // const configData = JSON.parse(process.env.CAPACITOR_CONFIG!);
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const builtWebAppDir = process.env.CAPACITOR_WEB_DIR!;
+  const destDir = join(usersProjectDir, 'electron', 'app');
+  try {
+    if (existsSync(destDir)) removeSync(destDir);
+    taskInfoMessageProvider(`Copying ${builtWebAppDir} into ${destDir}`);
+    copySync(builtWebAppDir, destDir);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
