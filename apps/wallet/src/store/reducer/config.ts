5f5f788e7908dee90c414f94aa715440fd57e986
Alireza Mousavi <alir.msvi@gmail.com>
2026-02-26 11:27:52 +0330

    hide balance & asset settings

--- apps/wallet/src/store/reducer/config.ts
+++ apps/wallet/src/store/reducer/config.ts
@@ -20,6 +20,8 @@ export const configInitialState: ConfigStateType = {
   multiSigLoadedTime: Date.now(),
   loadedPinType: '-',
   useActiveWallet: true,
+  hideBalances: false,
+  hideAssetsValues: false,
   network: {
     mainnet: {
       backend: NETWORK_BACKEND.EXPLORER,
@@ -42,6 +44,8 @@ export const configInitialState: ConfigStateType = {
 
 export type DisplayPayload = {
   display: DisplayType;
+  hideBalances?: boolean;
+  hideAssetsValues?: boolean;
 };
 
 export type CurrencyPayload = {
@@ -131,6 +135,14 @@ const configSlice = createSlice({
           ? state.useActiveWallet
           : action.payload.useActiveWallet;
     },
+    setHideBalances: (state, action: PayloadAction<boolean>) => {
+      state.hideBalances =
+        action.payload === undefined ? state.hideBalances : action.payload;
+    },
+    setHideAssetsValues: (state, action: PayloadAction<boolean>) => {
+      state.hideAssetsValues =
+        action.payload === undefined ? state.hideAssetsValues : action.payload;
+    },
     setConfig: (state, action: PayloadAction<ConfigPayload>) => {
       state.display = action.payload.display;
       state.currency = action.payload.currency;
@@ -140,6 +152,8 @@ const configSlice = createSlice({
       state.loadedPinType = action.payload.pinType;
       state.network.mainnet = action.payload.network.mainnet;
       state.network.testnet = action.payload.network.testnet;
+      state.hideBalances = action.payload.hideBalances ?? false;
+      state.hideAssetsValues = action.payload.hideAssetsValues ?? false;
     },
     setPinConfig: (state, action: PayloadAction<PinPayload>) => {
       state.pin.hasPin =
@@ -172,6 +186,8 @@ export const {
   setNodeUrl,
   setExplorerUrl,
   setActiveWallet,
+  setHideBalances,
+  setHideAssetsValues,
   setConfig,
   setMultiSigLoadedTime,
   setPinConfig,
