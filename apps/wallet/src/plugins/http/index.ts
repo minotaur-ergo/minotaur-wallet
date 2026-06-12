1c9ab625b0f1f3e34d96f73a8eed8cf7a53ef149
Alireza Mousavi <alir.msvi@gmail.com>
2026-06-12 18:15:08 +0330

    remove old plugin

--- apps/wallet/src/plugins/http/index.ts
+++
@@ -1,9 +0,0 @@
-export { CapacitorHttp } from './web';
-export type {
-  CapacitorHttpPlugin,
-  HttpHeaders,
-  HttpOptions,
-  HttpParams,
-  HttpResponse,
-  HttpResponseType,
-} from './definition';
